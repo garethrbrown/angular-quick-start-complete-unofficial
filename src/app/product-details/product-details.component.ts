06aa8011f1987f4635296325b2b904f5411587b3
Gareth Brown <gareth.brown@appsoftware.com>
2021-03-08 18:14:14 +0000

    Added cart service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
     selector: "app-product-details",
@@ -10,10 +11,11 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 
-    product:any;
+    public product:any;
 
     constructor(
         private route: ActivatedRoute,
+        private cartService: CartService
     ) { }
 
     ngOnInit() {
@@ -23,5 +25,10 @@ export class ProductDetailsComponent implements OnInit {
     
       // Find the product that correspond with the id provided in route.
       this.product = products.find(product => product.id === productIdFromRoute);
+    }
+
+    addToCart(product:any) {
+      this.cartService.addToCart(product);
+      window.alert('Your product has been added to the cart!');
     }
   }
